64eaa603d88ce7b5ed6d5ccf8feb4e707a07a280
olusogaolabisi <dreyz.bsu@gmail.com>
2022-09-12 10:48:17 -0400

    Equal parity

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-console.log('hello world!')
+// console.log('hello world!')
+//
+// let a = 2;
+// let b = 4;
+// let sum = a + b;
+// console.log(sum)
 
-let a = 2;
-let b = 4;
-let sum = a + b;
-console.log(sum)
+function equalParity(a, b){
+    return(a % 2 === 0 && b % 2 === 0 || a % 2 !== 0 && b % 2 !== 0)? true : false;
+}
+
+console.log(equalParity(2, 4))
